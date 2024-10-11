7c3c8f4ec47136abb54c938cd7a27ac761dab91e
kanha-9770 <app3.nessco@gmail.com>
2024-10-11 13:43:24 +0530

    putting only image for deployement

--- src/components/ui/ImageSlider.tsx
+++ src/components/ui/ImageSlider.tsx
@@ -29,33 +29,13 @@ const ImageSlider: React.FC = () => {
 
   return (
     <div className="relative w-full mx-auto h-full">
-      {isLargeScreen && videoSources.length > 0 ? (
-        <video
-          ref={videoRef}
-          className="w-full h-full object-cover rounded-2xl"
-          autoPlay={true}
-          loop
-          muted
-          controls={false}
-          playsInline
-          preload="metadata" // Preload only metadata for faster loading on larger screens
-        >
-          {videoSources.map((source, index) => (
-            <source key={index} src={source.src} type={source.type} />
-          ))}
-          Your browser does not support the video tag.
-        </video>
-      ) : (
-        imageSources && (
-          <Image
+      <Image
             src="https://res.cloudinary.com/dj4jijw2a/image/upload/v1728375387/webphome_r4zgw2.webp" // Use a fallback image for smaller screens
             alt="Hero Image"
             fill
             priority
             className="w-full h-full object-cover rounded-2xl"
           />
-        )
-      )}
     </div>
   );
 };
