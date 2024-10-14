decc922d13420627979c6a2201b36b8b7780b73e
kanha-9770 <app3.nessco@gmail.com>
2024-10-14 11:16:03 +0530

    putting image after removing every flag for lcp

--- src/components/ui/ImageSlider.tsx
+++ src/components/ui/ImageSlider.tsx
@@ -1,29 +1,7 @@
-import { useRef, useEffect, useState } from "react";
-import data from "../Constants/hero.json";
+
 import Image from "next/image";
 
 const ImageSlider: React.FC = () => {
-  const homeData = data.find((item) => item.category === "HeroSection")?.data;
-  const videoSources = homeData?.video?.sources || [];
-  const imageSources = homeData?.images;
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [showVideo, setShowVideo] = useState(false);
-
-  useEffect(() => {
-    // Initially show the image for 10 seconds
-    const timer = setTimeout(() => {
-      setShowVideo(true); // Switch to video after 10 seconds
-      if (videoRef.current) {
-        videoRef.current.load(); // Load the video
-      }
-    }, 10000); // 5 seconds delay
-
-    return () => clearTimeout(timer); // Clear the timer on cleanup
-  }, []);
-
-  if (!videoSources.length && !imageSources?.length) {
-    return <p>No media available.</p>;
-  }
 
   return (
     <div className="relative w-full mx-auto h-full">
