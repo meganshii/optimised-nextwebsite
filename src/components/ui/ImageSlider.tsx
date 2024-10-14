398b359ba477b9468c90219bc4849ca917a92671
kanha-9770 <app3.nessco@gmail.com>
2024-10-14 11:55:19 +0530

    added nessco india image uploaded to hostinger

--- src/components/ui/ImageSlider.tsx
+++ src/components/ui/ImageSlider.tsx
@@ -6,7 +6,7 @@ const ImageSlider: React.FC = () => {
   return (
     <div className="relative w-full mx-auto h-full">
       <Image
-            src="https://res.cloudinary.com/dj4jijw2a/image/upload/v1728375387/webphome_r4zgw2.webp"
+            src="https://www.nesscoindia.com/Assets/images/resource/fully-automatic-paper-cup-making-machine.webp"
             alt="Hero Image"
             height={200}
             width={200}
