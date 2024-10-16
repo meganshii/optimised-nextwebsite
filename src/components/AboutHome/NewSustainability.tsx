d57898855af18d87dd39ec9a37429ac661ee619c
meganshii <app4.nessco@gmail.com>
2024-10-16 13:59:13 +0530

    deployment

--- src/components/AboutHome/NewSustainability.tsx
+++ src/components/AboutHome/NewSustainability.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
-// import { IoArrowRedoSharp } from "react-icons/io5";
+
 
 import {
   Carousel,
